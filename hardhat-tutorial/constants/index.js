3d9603f61c5052ff64e4dd17be4faed1c640a285
qaderi <qadari.444@gmail.com>
2022-03-16 20:41:28 +0430

    contract address changed

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x8cfd83C13481Cd9f933ff192533e0560095c8862";
+const WHITELIST_CONTRACT_ADDRESS = "0x1Ee580108F3C7208ca7eCC33eA92E55423703523";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-lac.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
